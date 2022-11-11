0d395d2fac57e7a6546b86c0c9a3af9875e6ac61
Luis Luvia <lluvia@lusade.net>
2022-11-11 15:29:29 -0700

    Remove build info from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
-	build: {
-		lib: {
-			entry: 'src/lib/index.ts',
-			name: 'SvelteSVGSpinners',
-			fileName: (format) => `svelte-svg-spinners.${format}.js`,
-		}
-	}
 })

5b4f4d2ed369071ad51213462869903028bd4ce9
Luis Luvia <lluvia@lusade.net>
2022-11-11 17:31:29 -0700

    Add Block SVGs

---
+++ src/lib/BlocksShuffleTwo.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	export let size = 24;
+	export let width = size;
+	export let height = size;
+	export let color = "currentColor";
+	export let dur = "0.6s";
+	export let viewBox = "0 0 24 24";
+	export let ariaBusy = false;
+	export let ariaHidden = false;
+	export let ariaLive: "off" | "polite" | "assertive" = "off";
+</script>
+
+<svg
+	width={width}
+	height={height}
+	class={$$props.class}
+	fill={color}
+	viewBox={viewBox}
+	aria-busy={ariaBusy}
+	aria-hidden={ariaHidden}
+	aria-live={ariaLive}
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect x="1" y="1" rx="1" width="10" height="10">
+		<animate
+			id="a"
+			begin="0;k.end"
+			attributeName="x"
+			{dur}
+			values="1;13"
+			fill="freeze"
+		/>
+		<animate
+			id="d"
+			begin="b.end"
+			attributeName="y"
+			{dur}
+			values="1;13"
+			fill="freeze"
+		/>
+		<animate
+			id="g"
+			begin="e.end"
+			attributeName="x"
+			{dur}
+			values="13;1"
+			fill="freeze"
+		/>
+		<animate
+			id="j"
+			begin="h.end"
+			attributeName="y"
+			{dur}
+			values="13;1"
+			fill="freeze"
+		/>
+	</rect>
+	<rect x="1" y="13" rx="1" width="10" height="10">
+		<animate
+			id="b"
+			begin="a.end"
+			attributeName="y"
+			{dur}
+			values="13;1"
+			fill="freeze"
+		/>
+		<animate
+			id="e"
+			begin="d.end"
+			attributeName="x"
+			{dur}
+			values="1;13"
+			fill="freeze"
+		/>
+		<animate
+			id="h"
+			begin="g.end"
+			attributeName="y"
+			{dur}
+			values="1;13"
+			fill="freeze"
+		/>
+		<animate
+			id="k"
+			begin="j.end"
+			attributeName="x"
+			{dur}
+			values="13;1"
+			fill="freeze"
+		/>
+	</rect>
+</svg>
---
+++ src/lib/BlocksWave.svelte
@@ -0,0 +1,235 @@
+<script lang="ts">
+	export let size = 24;
+	export let width = size;
+	export let height = size;
+	export let color = "currentColor";
+	export let dur = "0.6s";
+	export let viewBox = "0 0 24 24";
+	export let ariaBusy = false;
+	export let ariaHidden = false;
+	export let ariaLive: "off" | "polite" | "assertive" = "off";
+</script>
+
+<svg
+	width={width}
+	height={height}
+	class={$$props.class}
+	fill={color}
+	viewBox={viewBox}
+	aria-busy={ariaBusy}
+	aria-hidden={ariaHidden}
+	aria-live={ariaLive}
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect x="1" y="1" width="7.33" height="7.33">
+		<animate
+			id="a"
+			begin="0;b.end+0.2s"
+			attributeName="x"
+			{dur}
+			values="1;4;1"
+		/>
+		<animate begin="0;b.end+0.2s" attributeName="y" {dur} values="1;4;1" />
+		<animate
+			begin="0;b.end+0.2s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="0;b.end+0.2s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="8.33" y="1" width="7.33" height="7.33">
+		<animate
+			begin="a.begin+0.1s"
+			attributeName="x"
+			{dur}
+			values="8.33;11.33;8.33"
+		/>
+		<animate begin="a.begin+0.1s" attributeName="y" {dur} values="1;4;1" />
+		<animate
+			begin="a.begin+0.1s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.1s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="1" y="8.33" width="7.33" height="7.33">
+		<animate begin="a.begin+0.1s" attributeName="x" {dur} values="1;4;1" />
+		<animate
+			begin="a.begin+0.1s"
+			attributeName="y"
+			{dur}
+			values="8.33;11.33;8.33"
+		/>
+		<animate
+			begin="a.begin+0.1s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.1s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="15.66" y="1" width="7.33" height="7.33">
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="x"
+			{dur}
+			values="15.66;18.66;15.66"
+		/>
+		<animate begin="a.begin+0.2s" attributeName="y" {dur} values="1;4;1" />
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="8.33" y="8.33" width="7.33" height="7.33">
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="x"
+			{dur}
+			values="8.33;11.33;8.33"
+		/>
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="y"
+			{dur}
+			values="8.33;11.33;8.33"
+		/>
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="1" y="15.66" width="7.33" height="7.33">
+		<animate begin="a.begin+0.2s" attributeName="x" {dur} values="1;4;1" />
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="y"
+			{dur}
+			values="15.66;18.66;15.66"
+		/>
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="15.66" y="8.33" width="7.33" height="7.33">
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="x"
+			{dur}
+			values="15.66;18.66;15.66"
+		/>
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="y"
+			{dur}
+			values="8.33;11.33;8.33"
+		/>
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="8.33" y="15.66" width="7.33" height="7.33">
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="x"
+			{dur}
+			values="8.33;11.33;8.33"
+		/>
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="y"
+			{dur}
+			values="15.66;18.66;15.66"
+		/>
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.3s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+	<rect x="15.66" y="15.66" width="7.33" height="7.33">
+		<animate
+			id="b"
+			begin="a.begin+0.4s"
+			attributeName="x"
+			{dur}
+			values="15.66;18.66;15.66"
+		/>
+		<animate
+			begin="a.begin+0.4s"
+			attributeName="y"
+			{dur}
+			values="15.66;18.66;15.66"
+		/>
+		<animate
+			begin="a.begin+0.4s"
+			attributeName="width"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+		<animate
+			begin="a.begin+0.4s"
+			attributeName="height"
+			{dur}
+			values="7.33;1.33;7.33"
+		/>
+	</rect>
+</svg>

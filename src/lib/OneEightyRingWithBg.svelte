1c7d0bdf79d3e3d74453389e330fc4e98510159e
Luis Luvia <lluvia@lusade.net>
2022-11-11 18:43:03 -0700

    Add Ninety, OneEighty, Pulse, PulseRing SVGs

---
+++ src/lib/OneEightyRingWithBg.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let size = 24
+	export let width = size
+	export let height = size
+	export let color = 'currentColor'
+	export let dur = '0.6s'
+	export let viewBox = '0 0 24 24'
+	export let ariaBusy = false
+	export let ariaHidden = false
+	export let ariaLive: 'off' | 'polite' | 'assertive' = 'off'
+</script>
+
+<svg
+	{width}
+	{height}
+	class={$$props.class}
+	fill={color}
+	{viewBox}
+	aria-busy={ariaBusy}
+	aria-hidden={ariaHidden}
+	aria-live={ariaLive}
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+		opacity=".25"
+	/>
+	<path
+		d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+	>
+		<animateTransform
+			attributeName="transform"
+			type="rotate"
+			{dur}
+			values="0 12 12;360 12 12"
+			repeatCount="indefinite"
+		/>
+	</path>
+</svg>
---
+++ src/lib/PulseMultiple.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	export let size = 24
+	export let width = size
+	export let height = size
+	export let color = 'currentColor'
+	export let dur = '0.6s'
+	export let viewBox = '0 0 24 24'
+	export let ariaBusy = false
+	export let ariaHidden = false
+	export let ariaLive: 'off' | 'polite' | 'assertive' = 'off'
+</script>
+
+<svg
+	{width}
+	{height}
+	class={$$props.class}
+	fill={color}
+	{viewBox}
+	aria-busy={ariaBusy}
+	aria-hidden={ariaHidden}
+	aria-live={ariaLive}
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<circle cx="12" cy="12" r="0">
+		<animate
+			id="a"
+			begin="0;c.end"
+			attributeName="r"
+			calcMode="spline"
+			{dur}
+			values="0;11"
+			keySplines=".52,.6,.25,.99"
+			fill="freeze"
+		/>
+		<animate
+			begin="0;c.end"
+			attributeName="opacity"
+			calcMode="spline"
+			{dur}
+			values="1;0"
+			keySplines=".52,.6,.25,.99"
+			fill="freeze"
+		/>
+	</circle>
+	<circle cx="12" cy="12" r="0">
+		<animate
+			id="b"
+			begin="a.begin+0.2s"
+			attributeName="r"
+			calcMode="spline"
+			{dur}
+			values="0;11"
+			keySplines=".52,.6,.25,.99"
+			fill="freeze"
+		/>
+		<animate
+			begin="a.begin+0.2s"
+			attributeName="opacity"
+			calcMode="spline"
+			{dur}
+			values="1;0"
+			keySplines=".52,.6,.25,.99"
+			fill="freeze"
+		/>
+	</circle>
+	<circle cx="12" cy="12" r="0">
+		<animate
+			id="c"
+			begin="a.begin+0.4s"
+			attributeName="r"
+			calcMode="spline"
+			{dur}
+			values="0;11"
+			keySplines=".52,.6,.25,.99"
+			fill="freeze"
+		/>
+		<animate
+			begin="a.begin+0.4s"
+			attributeName="opacity"
+			calcMode="spline"
+			{dur}
+			values="1;0"
+			keySplines=".52,.6,.25,.99"
+			fill="freeze"
+		/>
+	</circle>
+</svg>
